fcfbcc7627960e3abd2e6970ea11164a309e7e63
Augusto <sistemas3@unisaudems.org.br>
2025-04-09 09:07:37 -0400

    no message

--- projeto-inicial-web/admin.php
+++ projeto-inicial-web/admin.php
@@ -1,3 +1,12 @@
+<?php
+    require "src/conexao-bd.php";
+    require "src/Modelo/Produto.php";
+    require "src/Repositorio/ProdutoRepositorio.php";
+
+    $produtoRepositorio = new ProdutoRepositorio($pdo);
+    $produtos = $produtoRepositorio->buscarTodos();
+?>
+
 <!doctype html>
 <html lang="pt-br">
 <head>
@@ -27,56 +36,27 @@
   <section class="container-table">
     <table>
       <thead>
-        <tr>
-          <th>Produto</th>
-          <th>Tipo</th>
-          <th>Descricão</th>
-          <th>Valor</th>
-          <th colspan="2">Ação</th>
-        </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <td>Bife</td>
-        <td>Almoço</td>
-        <td>Delicioso prato</td>
-        <td>R$ 25.00</td>
-        <td><a class="botao-editar" href="editar-produto.html">Editar</a></td>
-        <td>
-          <form>
-            <input type="button" class="botao-excluir" value="Excluir">
-          </form>
-        </td>
-        
-      </tr>
-      <tr>
-        <td>Frango</td>
-        <td>Almoço</td>
-        <td>Delicioso prato</td>
-        <td>R$ 25.00</td>
-        <td><a class="botao-editar" href="editar-produto.html">Editar</a></td>
-        <td>
-          <form>
-            <input type="button" class="botao-excluir" value="Excluir">
-          </form>
-        </td>
-      </tr>
-      <tr>
-        <td>Café Gelado</td>
-        <td>Café</td>
-        <td>Delicioso prato</td>
-        <td>R$ 25.00</td>
-        <td><a class="botao-editar" href="editar-produto.html">Editar</a></td>
-        <td>
-          <form>
-            <input type="button" class="botao-excluir" value="Excluir">
-          </form>
-        </td>
-      </tr>
+        <?php foreach ($produtos as $produto): ?>
+          <tr>
+            <td><?= $produto->getNome() ?></td>
+            <td><?= $produto->getTipo() ?></td>
+            <td><?= $produto->getDescricao() ?></td>
+            <td><?= $produto->getPrecoFormatado() ?></td>
+            <td>
+              <a class="botao-editar" href="editar-produto.html">Editar</a>
+            </td>
+            <td>
+              <form action="excluir-produto.php" method="post">
+                <input type="hidden" name="id" value="<?= $produto->getId() ?>">
+                <input type="submit" class="botao-excluir" value="Excluir">
+              </form>
+            </td>
+          </tr>
+        <?php endforeach; ?>
       </tbody>
     </table>
-  <a class="botao-cadastrar" href="cadastrar-produto.html">Cadastrar produto</a>
-  <form action="#" method="post">
+  <a class="botao-cadastrar" href="cadastrar-produto.php">Cadastrar produto</a>
+  <form method="post">
     <input type="submit" class="botao-cadastrar" value="Baixar Relatório"/>
   </form>
   </section>
